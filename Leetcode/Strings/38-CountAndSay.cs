3e42493e2f595e98d11ee149aa164f162f79c7a3
Ram Kumaran <ramz.creativity@Gmail.com>
2021-06-29 23:43:13 +0530

    Create 38-CountAndSay.cs
    LeetCode #38 - Count and Say - Recursive

---
+++ Leetcode/Strings/38-CountAndSay.cs
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/count-and-say/
+// 38 : Count and Say
+// Difficulty : Medium
+// Recursive
+public class Solution {
+    public string CountAndSay(int n) {
+        if(n <= 1)
+            return "1";
+        
+        string strResult = CountAndSay(n-1);
+        
+        StringBuilder sb = new StringBuilder();
+        int counter = 1;
+        for(int idx = 1; idx <= strResult.Length; idx++)
+        {
+            if(idx < strResult.Length && strResult[idx] == strResult[idx-1])
+                counter++;
+            else
+            {
+                sb.Append(counter).Append(strResult[idx-1]);
+                counter = 1;
+            }
+        }
+        return sb.ToString();
+    }
+}
